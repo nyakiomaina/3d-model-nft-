36b7ff79efa9db55fd5000b227bf30c0571361aa
Abdurhaman Nur <158743046+abdunur-dev@users.noreply.github.com>
2025-10-30 07:40:12 +0300

    Update contract.ts

--- lib/web3/contract.ts
+++ lib/web3/contract.ts
@@ -110,7 +110,7 @@ export async function getAllRooms(): Promise<Room[]> {
           description: `A ${room.colorTheme} themed room`,
           objects,
           createdAt: new Date(room.timestamp).toISOString(),
-          creatorAddress: "0x79DCbB57F9394AD88b4be4180d4B72BA058cd69d",
+          creatorAddress: "0x000000000000000000000000000000000000000",
           nftTokenId: room.id || index.toString(),
         }
       })
